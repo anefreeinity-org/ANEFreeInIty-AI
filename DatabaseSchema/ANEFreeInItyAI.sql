10a0ed63f65fb19f4934d3f50c49260c511fea15
anefreeinity <67321022+anefreeinity@users.noreply.github.com>
2023-02-13 02:38:54 +0530

    database modified

--- DatabaseSchema/ANEFreeInItyAI.sql
+++ DatabaseSchema/ANEFreeInItyAI.sql
@@ -22,7 +22,9 @@ CREATE TABLE project (
 CREATE TABLE project_iteam_mapper (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     project_id INT NOT NULL,
-    vector2d_id INT NOT NULL
+    vector2d_id INT NOT NULL,
+    vector2d_name VARCHAR(30) NOT NULL,
+    iteam_status INT NOT NULL
 );
 
 INSERT INTO vector2d VALUES (1, 3, 4.0, 5.0, 53.0, "Vector A" "demo vector", 1);
@@ -31,6 +33,6 @@ INSERT INTO vector2d VALUES (2, 4.0, 3.0, 5.0, 36.0, "Vector B" "demo vector", 1
 INSERT INTO project VALUES (1, "project 1", "demo project");
 INSERT INTO project VALUES (2, "project 2", "demo project");
 
-INSERT INTO project_iteam_mapper VALUES (1, 1, 8);
-INSERT INTO project_iteam_mapper VALUES (2, 1, 9);
-INSERT INTO project_iteam_mapper VALUES (3, 2, 4);
+INSERT INTO project_iteam_mapper VALUES (1, 1, 10, "Vector E", 0);
+INSERT INTO project_iteam_mapper VALUES (2, 1, 11, "Vector F", 0);
+INSERT INTO project_iteam_mapper VALUES (3, 2, 12, "Vector G", 0);
